df84d88cce752da03e711ec1e29e9d0b960a2dd4
VictorPachecoAzanr <vpachecoaznar@gmail.com>
2025-05-28 22:06:50 +0200

    added SAM testing

--- test/test_SAM.py
+++ test/test_SAM.py
@@ -1,6 +1,7 @@
 import os
-from apb_spatial_computer_vision.lang_sam_utilities import LangSAM_apb
+from apb_spatial_computer_vision.sam_utilities import SamGeo_apb
 from apb_spatial_computer_vision.raster_utilities import Ortophoto
+from apb_spatial_computer_vision.main import pyramid_sam_apply
 from apb_spatial_computer_vision import *
 from BETA.text_prompts import text_to_bbox_lowres_complete
 from functools import partial
@@ -8,28 +9,28 @@ import time
 
 import unittest
 
-class TestDINO(unittest.TestCase):
-    test_folder: str
+class TestSAM(unittest.TestCase):
+    vector_file: str
     path_orto: str
     complete_image: Ortophoto
-    text_prompt: str
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.test_folder =  os.getenv('DATA_DIR', os.path.join(os.path.dirname(__file__), '..', 'data'))
         cls.path_orto = os.path.join(cls.test_folder, os.getenv('NAME_ORTOFOTO', 'ORTO_ME_BCN_2023.tif'))
-        cls.text_prompt = os.getenv('TEXT_PROMPT','building')
-        vector_file=os.getenv('VECTOR_FILE',None)
-        if vector_file is not None:
-            cls.output_file=os.path.join(cls.test_folder, vector_file)
-        else:
-            cls.output_file=vector_file
         cls.complete_image = Ortophoto(cls.path_orto)
+        cls.vector_file = os.path.join(cls.complete_image.folder,os.getenv('VECTOR_FILE',''))
 
-    def test_text_prompt(self):
+
+    def test_sam_from(self):
+        sam = SamGeo_apb(
+        model_type="vit_h",
+        automatic=False,
+        sam_kwargs=None,
+        )
         self.complete_image.pyramid=os.path.join(self.complete_image.folder,os.path.splitext(self.complete_image.basename)[0]+'_pyramid')
         t0=time.time()
-        text_to_bbox_lowres_complete(self.complete_image,self.text_prompt,output=self.output_file)
+        pyramid_sam_apply(self.complete_image,self.vector_file,1024,'geom',1,sam)
         t1=time.time()
 
 
