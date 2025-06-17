f11d49fc946a57c1575279932c91c15db14d0d10
VictorPachecoAznar <156831533+VictorPachecoAznar@users.noreply.github.com>
2025-06-18 00:07:30 +0200

    Create mkdocs website
    * Update docs.yml
    
    * call docs from conda env
    
    * deocoupled mkdocs index automations from package
    
    * added space on docs.yml
    
    * mkdocs separate build and deploy
    
    * change to main branch

--- docs/automate_index_md.py
+++ docs/automate_index_md.py
@@ -1,10 +1,8 @@
 import os
-from apb_spatial_computer_vision.main import folder_check
 package_dir = "apb_spatial_computer_vision"
 docs_dir = os.path.dirname(__file__)
-api_dir=folder_check(os.path.join(docs_dir,'api'))
-
-os.makedirs(docs_dir, exist_ok=True)
+api_dir=os.path.join(docs_dir,'api')
+os.makedirs(api_dir,exist_ok=True)
 
 modules = [f[:-3] for f in os.listdir(package_dir) if f.endswith(".py") and f != "__init__.py"]
 
@@ -14,3 +12,4 @@ for module in modules:
         f.write("# API Reference\n\n")
         f.write(f"## {module}\n\n")
         f.write(f"::: {package_dir}.{module}\n\n")
+
